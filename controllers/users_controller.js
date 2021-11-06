b2b2bc984e0e5358e0f810affaa22668569dcc8a
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-11-06 10:44:36 +0530

    added partials for header and footer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1>User profile</h1>');
+    // return res.end('<h1>User profile</h1>');
+    res.render('user_profile',{
+        title:"user profile"
+    });
 }
