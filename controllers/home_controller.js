f957541445a8c5715e172fae1286600cbf7dbe38
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-11-18 17:52:11 +0530

    display posts and name of poster in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,22 @@
+const Post = require('../models/post');
+
 module.exports.home=function(req,res){
     // return res.end('<h1>Express is up for codial</h1>');
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    
+    // Post.find({}, function(err, posts){
+    //     return res.render('home',{
+    //         title:"Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
 
-    return res.render('home',{
-        title:"home"
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts: posts
+        });
     });
 }
