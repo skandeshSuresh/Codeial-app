2ecbd2c87fc02cb98d0d1ec23d0b3677e1bd6202
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-11-20 21:32:14 +0530

    add code for deleting comments from posts

--- routes/comments.js
+++ routes/comments.js
@@ -8,5 +8,8 @@ const commentsController = require('../controllers/comments_controller');
 console.log('router loaded !!');
 
 router.post('/create', passport.checkAuthentication,  commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
+
+
 
 module.exports=router;
