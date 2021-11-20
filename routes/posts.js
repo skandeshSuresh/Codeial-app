e4969f89b5acbfc853f0e467a95bed370f06af36
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-11-20 20:24:58 +0530

    add code to delete posts

--- routes/posts.js
+++ routes/posts.js
@@ -8,5 +8,7 @@ const postsController = require('../controllers/posts_controller');
 console.log('router loaded !!');
 
 router.post('/create', passport.checkAuthentication,  postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
+
 
 module.exports=router;
