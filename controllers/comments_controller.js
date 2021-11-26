bea4513efd723064270f8cd802375e4088002c2c
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-11-26 19:16:44 +0530

    convert parts of code which handle posts creation and destruction to ajax

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -14,11 +14,13 @@ module.exports.create = async function(req, res){
         post.comments.push(comment);
         post.save();
 
-        res.redirect('/');
+        req.flash('success', 'Your comment has been added!');
+
+        return res.redirect('back');
     }
     }catch{
-        console.log('Error', err);
-        return;
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
@@ -31,12 +33,15 @@ module.exports.destroy = async function(req, res){
         comment.remove();
 
         let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+        req.flash('success', 'Your comment has been deleted!');
+
         return res.redirect('back');
     }else{
+        req.flash('error', 'You cannot delete this comment!');
         return res.redirect('back');
     }
     }catch(err){
-        console.log('Error', err);
-        return;
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -16,19 +16,20 @@ module.exports.home= async function(req,res){
     //populate the user of each post
     try{
         let posts = await Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    });
-    let users = await User.find({});
-    return res.render('home',{
-        title:"Codeial | Home",
-        posts: posts,
-        all_users: users
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
+        let users = await User.find({});
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts: posts,
+            all_users: users
+            });
 
     }catch(err){
         console.log('error', err);
