b73bcddf50d298c7406d937d73c49643f3398cda
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-11-26 10:55:59 +0530

    use noty js library to display flash messages for sign in, sign out, invalid credentials entered, creating a post, deleting a post, etc

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -6,18 +6,21 @@ const User = require('../models/user');
 
 //authentication using passport
 passport.use(new LocalStrategy({
-    usernameField: 'email'
+    usernameField: 'email',
+    passReqToCallback: true
 },
-function(email, password, done){
+function(req, email, password, done){
     //find user and establish identity
     User.findOne({email: email}, function(err,user){
         if(err){
-            console.log('error in finding the user --> passport');
+            // console.log('error in finding the user --> passport');
+            req.flash('error', err);
             return done(err);
         }
 
         if(!user || user.password != password){
-            console.log('invalid username/password');
+            // console.log('invalid username/password');
+            req.flash('error', 'Invalid username/password');
             return done(null, false);
         }
         return done(null, user);
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -13,11 +13,13 @@ module.exports.create = async function(req,res){
         //     }
         //     return res.redirect('back');
         });
+        req.flash('success', 'Post Published!');
         return res.redirect('back');
 
     }catch(err){
-        console.log('Error', err);
-        return;
+        // console.log('Error', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
@@ -29,14 +31,18 @@ module.exports.destroy = async function(req, res){
     if(post.user == req.user.id){
         post.remove();
 
-        await Comment.deleteMany({post: req.params.id}, function(err){
-            return res.redirect('back');
-        });
+        await Comment.deleteMany({post: req.params.id});
+
+        req.flash('success', 'Post and associated comments deleted!');
+
+        return res.redirect('back');
     }else{
+        req.flash('error', 'You cannot delete this post!');
         return res.redirect('back');
     }
     }catch(err){
-        console.log('Error', err);
-        return;
+        // console.log('Error', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
