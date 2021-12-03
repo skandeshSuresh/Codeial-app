e624ae351bbfe85cc79d3d2928517d3dddefc332
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-12-03 20:41:31 +0530

    add authentication and authorization to apis

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -25,7 +25,7 @@ module.exports.destroy = async function(req, res){
         let post = await Post.findById(req.params.id);
 
         //.id means converting object id into string
-    // if(post.user == req.user.id){
+    if(post.user == req.user.id){
         post.remove();
 
         await Comment.deleteMany({post: req.params.id});
@@ -34,11 +34,13 @@ module.exports.destroy = async function(req, res){
         return res.json(200, {
             message: "post and associated comments deleted successfully "
         });
-    // }else{
-    //     req.flash('error', 'You cannot delete this post!');
-    //     return res.redirect('back');
-    // }
+    }else{
+        return res.json(401, {
+            message: "you cannot delete this post"
+        })
+    }
     }catch(err){
+        console.log('*******', err);
         return res.json(500, {
             message: "Internal server error"
         });
