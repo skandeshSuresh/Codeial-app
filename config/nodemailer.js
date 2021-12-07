79a5ca4607f3a739d95a3795537a7e75fed2b337
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-12-07 18:43:07 +0530

    set up nodemailer for mailing functionality of the website

---
+++ config/nodemailer.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+
+//part of code that sends the emails...defines how comms is going to take place
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'skandeshsuresh@gmail.com',
+        pass: 's_k_a_n_desh1'
+    }
+});
+
+//defines html for the email to be sent
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+           if(err){
+               console.log('error in rendering template');
+               return;
+           } 
+
+           mailHTML = template;
+        }
+    )
+
+    return mailHTML;
+}
+
+module.exports={
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
